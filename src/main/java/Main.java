a060e7cc12a85aadcb01c5024d538d7eca5a4302
TheNiggo <nick200108@gmail.com>
2019-04-22 20:53:21 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Ask user after his name
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        // Reverse the user name
+        String reversedName = String.valueOf(new StringBuilder(name).reverse());
+
+        // Print reversed name
+        System.out.println("Your name in reverse is: " + reversedName);
+    }
+}
